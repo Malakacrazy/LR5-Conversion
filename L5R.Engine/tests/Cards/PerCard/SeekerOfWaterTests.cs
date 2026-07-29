c52b4bb57e53805f9cb1ab3498cc76709b629cfb
Malakacrazy <tete_us_2@hotmail.com>
2026-07-29 17:39:42 -0300

    Implement seeker-of-water scriptOverride
    Same shape as seeker-of-air, gated on the water trait instead.
    
    Part of a 7-script batch verified together; full suite: 1017/1017 passing.

---
+++ L5R.Engine/tests/Cards/PerCard/SeekerOfWaterTests.cs
@@ -0,0 +1,38 @@
+using L5R.Engine.Abilities;
+using L5R.Engine.Cards.Scripts;
+using L5R.Engine.State;
+
+namespace L5R.Engine.Tests.Cards.PerCard;
+
+public class SeekerOfWaterTests
+{
+    [Test]
+    public void WhenAWaterProvinceItControlsIsRevealed_GainsOneFate()
+    {
+        var p1 = new Player { Name = "Player1" };
+        var p2 = new Player { Name = "Player2" };
+        var game = new GameState { Player1 = p1, Player2 = p2, ActivePlayer = p1, CurrentPhase = Phase.Dynasty };
+        var seeker = new Card { Id = "seeker-of-water", Type = CardType.Role, Controller = p1 };
+        var province = new Card { Id = "some-province", Type = CardType.Province, Controller = p1, Traits = new[] { "water" } };
+
+        var context = new AbilityContext { Game = game, Player = p1, Source = seeker, Target = province };
+
+        new SeekerOfWaterGainFateOnMatchingProvinceReveal().Execute(context);
+
+        Assert.That(p1.Fate, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void WhenTheRevealedProvinceIsNotWater_Throws()
+    {
+        var p1 = new Player { Name = "Player1" };
+        var p2 = new Player { Name = "Player2" };
+        var game = new GameState { Player1 = p1, Player2 = p2, ActivePlayer = p1, CurrentPhase = Phase.Dynasty };
+        var seeker = new Card { Id = "seeker-of-water", Type = CardType.Role, Controller = p1 };
+        var province = new Card { Id = "some-province", Type = CardType.Province, Controller = p1, Traits = new[] { "earth" } };
+
+        var context = new AbilityContext { Game = game, Player = p1, Source = seeker, Target = province };
+
+        Assert.Throws<InvalidOperationException>(() => new SeekerOfWaterGainFateOnMatchingProvinceReveal().Execute(context));
+    }
+}
